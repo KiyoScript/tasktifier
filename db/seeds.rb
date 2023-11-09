087db84e02faea0a0f75e76d8af2ea5aafd25a1f
Pudding(Lloyd) <89757149+KiyoScript@users.noreply.github.com>
2023-11-09 10:05:29 +0800

    [TF] Implement User Table
    GitHub Issue: https://github.com/KiyoScript/tasktifier/issues/1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Generate Admin
+User.create(
+  username: 'KiyoScript',
+  email: 'KiyoScript@tasktifier.com',
+  gender: 'male',
+  role: 'admin',
+  password: 'KiyoScr1p5@MisY23',
+  password_confirmation: 'KiyoScr1p5@MisY23'
+)
