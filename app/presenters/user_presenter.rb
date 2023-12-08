1f8245857438b04e4105e9f25de20a2f46ca34b0
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2023-12-08 20:28:54 +0800

    [TF] Update username method

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -3,9 +3,6 @@ class UserPresenter < ApplicationPresenter
   def initialize(current_user)
     @user = current_user
   end
-  def username
-    @user.email.split('@')[0]
-  end
 
   def categories
     @user.categories
